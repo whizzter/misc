0921f11aeb3671e917062142267a0d6a4d9b1bd5
Jonas Lund <whizzter@gmail.com>
2018-11-28 04:21:45 +0100

    Made formatting of the test more explanatory, better names/comments and some extra notes to the README.md

--- basync/README.md
+++ basync/README.md
@@ -1,6 +1,10 @@
-BASYNC is a single header C++ async/await system based on the ideas of Simon Tathams coroutine hack for Putty.
+BASYNC (Bad ASYNC) is a single header C++ async/await system based on
+the ideas of Simon Tathams coroutine hack for Putty. (same "minor" weakness of not allowing BAWAIT inside a switch)
+
 Thanks to C++11 and lambdas this system is not tied to any static variables but can be freely instantiated.
 (although it does produce some memory allocations if that is an issue for users).
+
+Usage-wise it's patterned after the EcmaScript async/await based on promises.
 
 Licenced under a Zlib licence so acknowledgement is nice but not requierd.
 
--- basync/test.cpp
+++ basync/test.cpp
@@ -7,26 +7,28 @@
 
 // An asyncronous function needs the BASYNC macro (returntype, name/arg , variables, body)
 BASYNC(
-int , test(std::string id,int a,basync::promise<std::string> (*q)()),
-
-  int count;          // variable needs to be here to live across BAWAIT calls
-  std::string val;    // this variable is the return from an await!
+	int                 // return type
+,
+	test ( std::string id, int count, std::function<basync::promise<std::string>()> valuesource)
+,
+	int         idx;    // variable needs to be here to live across BAWAIT calls
+	std::string val;    // this variable is the return value from an await!
 ,
 	std::cout<<"["<<id<<"] Entering\n";
-	for (count=0;count<a;count++) {
-		val=std::to_string(count);
+	for (idx=0;idx<count;idx++) {
+		val=std::to_string(idx);
 
-		BAWAIT(val,q());
+		BAWAIT(val,valuesource()); // <-- This invocation of the BAWAIT macro will PAUSE the function until a value is provided
 
 		int dummy=123;;
-		std::cout<<"["<<id<<"] Test awaited value:"<<val<<" on iter "<<count<<" (stack:"<<((void*)&dummy) << ")\n";
+		std::cout<<"["<<id<<"] Test awaited value:"<<val<<" on iter "<<idx<<" (stackpos:"<<((void*)&dummy) << ")\n";
 	}
 	std::cout<<"["<<id<<"] Finished\n";
-	return a*20;
+	return count*20;
 )
 // END OF ASYNC function
 
-// predeclaration of 2 methods that produces 
+// predeclaration of 2 methods that produces promises that can be BAWAIT:ed on
 basync::promise<std::string> direct();
 basync::promise<std::string> pendingprod();
 
@@ -35,41 +37,47 @@ std::vector<basync::promise<std::string>> pendingvalues;
 
 int main(int argc,char **argv) {
 
+	std::cout<<"{Main  } Entering\n";
+
 	// storage for 2 promises
 	basync::promise<int> proms[3];
 	// first create one that will fetch pending promises that won't be resolved immediately
-	proms[0]=test("First",2,pendingprod);
+	proms[0]=test("First ",2,pendingprod);
 	// the second will only request promises that will be fulfilled immediately
 	proms[1]=test("Second",4,direct);
 	// the third one that will also fetch pending promises that won't be resolved immediately but live longer than number 0
-	proms[2]=test("Third",6,pendingprod);
+	proms[2]=test("Third ",6,pendingprod);
 
 	// register handlers
 	for (int i=0;i<3;i++) {
 		proms[i].then([=](int v){
-			std::cout<<"Result ["<<(i+1)<<"] gotten as:"<<v<<"\n";
+			std::cout<<"{Main  } Result ["<<(i+1)<<"] gotten as:"<<v<<"\n";
 		});
 	}
 
 	int count=0;
 	while(pendingvalues.size()) {
 		// print out status of our possibly pending promises
-		std::cout<<"Pending values:"<<pendingvalues.size()<<" Done:["<<proms[0].done()<<","<<proms[1].done()<<","<<proms[2].done()<<"]\n";
-		// produce a value from the mainloop to the async
-		pendingvalues[0].resolve("From main loop"+std::to_string(count++));
+		std::cout<<"{Main  } Pending values:"<<pendingvalues.size()<<" Done:["<<proms[0].done()<<","<<proms[1].done()<<","<<proms[2].done()<<"]\n";
+		
+		// produce a value from the mainloop to the async in front if needed
+		pendingvalues[0].resolve("{From main loop "+std::to_string(count++)+"}");
+
 		// erase the head as it's produced already!
 		pendingvalues.erase(pendingvalues.begin());
 	}
 
-	std::cout<<"Done!!";
+	std::cout<<"{Main  } Finished (ALL DONE!!!)";
 
 	return 0;
 }
 
+// this function just produces values immediately
 basync::promise<std::string> direct() {
 	return "direct resolve!";
 }
 
+// whilst this function creates a pending value into the pendingvalues array that needs to be resolved later
 basync::promise<std::string> pendingprod() {
 	// make an promise that isn't finalized yet.
 	pendingvalues.emplace_back();
